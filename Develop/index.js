0d462dfc775b2a74e3238954194b0eb797ff6208
Kameron <kfarshchian@gmail.com>
2022-12-28 16:09:57 -0700

    start adding questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,8 +1,15 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
-const questions = [];
-
+const questions = () =>{ 
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'projectName',
+            message: 'What is your project name?',
+        },
+    ]);
+};
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
