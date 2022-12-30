92b63817374c318599c032da4ed90e59dd4fca2c
Kameron <kfarshchian@gmail.com>
2022-12-30 00:38:20 -0700

    added more questions and added checkbox to license and trying to figure out what licenses to add and what all the badges are

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,8 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require("./utils/generateMarkdown");
+
 // TODO: Create an array of questions for user input
 const questions = () =>{ 
     return inquirer.prompt([
@@ -12,7 +14,7 @@ const questions = () =>{
         {
             type: 'input',
             name: 'description',
-            message: 'Please describe your project?',
+            message: 'Please describe what, why, and how for this project? - What was your motivation? - Why did you build this project? - What problem does it solve? - What did you learn?',
         },
         {
             type: 'input',
@@ -25,14 +27,20 @@ const questions = () =>{
             message: 'What needs to be installed in order to work properly?',
         },
         {
-            type: 'input',
+            type: 'checkbox',
             name: 'license',
-            message: 'What license will this be under?',
+            message: 'What license will this be under?(Use space bar to select license)',
+            choices: ['HTML', 'CSS', 'JavaScript', 'MySQL',],
         },
         {
             type: 'input',
             name: 'credits',
             message: 'Who all contributed to this project. If it was only one person N/A can be applied?',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'What is the purpose of the project?',
         },
         {
             type: 'input',
@@ -44,13 +52,24 @@ const questions = () =>{
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
     console.log(fileName, data);
-    return fs.writeFileSync(path.join(process.cwd(), fileName), readmeData);
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
 }
 
 
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    questions()
+    .then((data) => {
+        console.log(data);
+        return fs.writeFileSync("./readme.md", generateMarkdown(data));
+      })
+      .catch((err) => {
+        if (err) {
+          throw err;
+        }
+      });
+}
 
 // Function call to initialize app
 init();
